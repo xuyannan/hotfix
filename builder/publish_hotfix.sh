bf71a7cf75235c3df6c43172d3e2a5ccd9fe3917
xuyannan <xuyannan@lancai.cn>
2017-11-17 13:56:22 +0800

    hotfix

--- builder/publish_hotfix.sh
+++ builder/publish_hotfix.sh
@@ -2,13 +2,14 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
 if [[ -z "$1" ]]
 then
     echo "要输入一个分支名(不需要hotfix前缀)"
 else
-    echo '${GREEN}check git status:'
+    echo "${GREEN}check git status:"
     UNSTAGED_DIFF="$(git status)"
-    echo "${UNSTAGED_DIFF}"
+    echo "${NC}${UNSTAGED_DIFF}"
     if [[ $UNSTAGED_DIFF =~ working\ tree\ clean$ ]]
     then 
 	echo "${NC}branch is clean"
@@ -16,10 +17,10 @@ else
 	echo "${RED}有未提交的改动，操作取消"
 	exit 0
     fi
-    echo '1. pull master:'
+    echo "${GREEN}1. pull master:${NC}"
     git pull origin master
-    echo '2. meger master:'
-    # git merge master
+    echo "${GREEN}2. meger master"
+    git merge master
     # echo '3. commit hotfix branch:'
     # git commit
     # echo '4. switch to master:'
