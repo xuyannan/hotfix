4d75217ff8ff6e73936b3711b29adaa13eb1ab76
xuyannan <xuyannan@lancai.cn>
2017-11-17 14:15:03 +0800

    tag

--- builder/publish_hotfix.sh
+++ builder/publish_hotfix.sh
@@ -5,7 +5,8 @@ NC='\033[0m'
 
 if [[ -z "$1" ]]
 then
-    echo "要输入一个分支名(不需要hotfix前缀)"
+    echo "${RED}要输入一个分支名(不需要hotfix前缀)${NC}"
+    exit 1
 else
     echo "${GREEN}check git status:"
     UNSTAGED_DIFF="$(git status)"
@@ -21,12 +22,16 @@ else
     git pull origin master
     echo "${GREEN}2. meger master${NC}"
     git merge master
-    # echo '3. commit hotfix branch:'
-    # git commit
-    # echo '4. switch to master:'
-    # git checkout master
-    # echo '5. merge hotfix to master:'
-    # git merge hotfix_$1
-    # echo '6. push master:'
-    # git push -u origin master
+    echo "${GREEN}3. commit hotfix branch:${NC}"
+    git commit
+    echo "${GREEN}4. switch to master:${NC}"
+    git checkout master
+    echo "${GREEN}5. merge hotfix to master:${NC}"
+    git merge hotfix_$1
+    echo "${GREEN}6. tag:${NC}"
+    node tag.js --m
+    echo "${GREEN}6. push master:${NC}"
+    git push -u origin master --tag
+    echo "${GREEN}done${NC}"
+    exit 0
 fi
